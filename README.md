177fd8b70d800973edf34b4983d88ef954701bac
twolak88 <twolak88@gmail.com>
2020-10-03 15:52:28 +0200

    Create initial project #1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![CircleCI](https://circleci.com/gh/twolak88/mssc-beer-inventory-service.svg?style=svg&circle-token=1ed6c905a022527d5ed6b8a082b4cbb98ce6229c)](https://app.circleci.com/pipelines/github/twolak88/mssc-beer-inventory-service?branch=main)
+
 # mssc-beer-inventory-service
 MSSC Beer Inventory Service
---
+++ src/main/java/twolak/springframework/beer/inventory/service/MsscBeerInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package twolak.springframework.beer.inventory.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerInventoryServiceApplication.class, args);
+	}
+
+}
